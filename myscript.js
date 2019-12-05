bd78cb4f3c54aebca4b6d5a1badc69d7fcc7f2f1
we <we@2019120009.local>
2019-12-05 18:37:36 +0900

    add check box

--- myscript.js
+++ myscript.js
@@ -2,12 +2,26 @@ function addData(data,id){
   if(data != 'write your works' && data != '' ){
     var mainList = document.getElementById(id);
     var elem = document.createElement("li");
+    var tmp = document.createElement("div");
+    tmp.className = 'check'
+
     elem.innerHTML=data;
     elem.ondblclick = function(){
       if(id == 'work_list')
         addData(data,"done_list");
-      this.parentNode.removeChild(this);
+      this.parentNode.parentNode.removeChild(this.parentNode);
     };
-    mainList.appendChild(elem);
+
+    tmp.appendChild(elem);
+    tmp.appendChild(createNewCheckboxt('name','id'))
+    mainList.appendChild(tmp);
   }
+}
+
+function createNewCheckboxt(name, id){
+    var checkbox = document.createElement('input');
+    checkbox.type= 'checkbox';
+    checkbox.name = name;
+    checkbox.id = id;
+    return checkbox;
 }
