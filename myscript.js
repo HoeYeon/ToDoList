9d1003afd0fcc925eb73797555e6537971ecb34b
HoeYeon <shy9546@naver.com>
2019-12-29 17:44:38 +0900

    save ToDoList

--- myscript.js
+++ myscript.js
@@ -1,4 +1,19 @@
 
+let toDos = [];
+let countID = 1;
+const TODO_LS = "toDOs";
+function saveToDo(){
+  localStorage.setItem(TODO_LS,JSON.stringify(toDos));
+}
+function loadToDo(){
+  const loadedToDo = localStorage.getItem(TODO_LS);
+  if (loadedToDo !== null){
+    const parsedToDo = JSON.parse(loadedToDo);
+    parsedToDo.forEach(function(todo){
+      addData(todo.text,todo.loc);
+    })
+  }
+}
 function addData(data,id){
   if(data != 'write your works' && data != '' ){
     let mainList = document.querySelector("#"+id);
@@ -8,25 +23,42 @@ function addData(data,id){
     tmp.className = 'check';
 
     elem.innerHTML=data;
-    elem.ondblclick = function(){deleteNode(elem,data,id);};
+    elem.id = countID;
+    //elem.ondblclick = function(){deleteNode(elem,data,id);};
     check.onclick = function(){deleteNode(elem,data,id);};
+    elem.appendChild(check);
     tmp.appendChild(elem);
-    tmp.appendChild(check);
     mainList.appendChild(tmp);
+
+    const toDosObj = {
+      text:data,
+      ids: countID,
+      loc: id
+    }
+    toDos.push(toDosObj);
+    countID++;
+    saveToDo();
   }
 }
 function createNewCheckbox(){
     let checkbox = document.createElement('input');
     checkbox.type= 'checkbox';
-    // checkbox.name = name;
+    checkbox.className = 'checkbox';
     // checkbox.id = id;
     return checkbox;
 }
 
 function deleteNode(elem,data,id){
-  if(id == 'work_list')
-    addData(data,"done_list");
+  if(id == 'work_list'){
+    addData(data,'done_list');
+  }
   elem.parentNode.parentNode.removeChild(elem.parentNode);
+  const li = event.target.parentNode;
+    const cleanToDos = toDos.filter(function(toDo){
+      return toDo.ids !== parseInt(li.id);
+    })
+    toDos = cleanToDos;
+    saveToDo();
 }
 
 function listenEnter(){
@@ -37,3 +69,4 @@ function listenEnter(){
     }
 });
 }
+loadToDo();
